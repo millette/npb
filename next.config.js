067e4bb331fab51c9fab8f2067d7841f593c5e0f
Robin Millette <robin@millette.info>
2019-05-25 00:10:23 -0400

    Bundle analyzer config.

--- next.config.js
+++ next.config.js
@@ -1,2 +1,34 @@
-const withBundleAnalyzer = require("@next/bundle-analyzer")({ enabled: process.env.ANALYZE === "true" })
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+  // would be nice to support more webpack-bundle-analyzer options like:
+  openAnalyzer: false,
+  generateStatsFile: true
+})
 module.exports = withBundleAnalyzer({})
+
+/*
+// @next/bundle-analyzer would be something like:
+
+module.exports = (pluginConfig = {}) => (nextConfig = {}) => {
+  const { enabled = true } = pluginConfig
+  return Object.assign({}, nextConfig, {
+    webpack (config, options) {
+      if (enabled) {
+        const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
+        config.plugins.push(
+          new BundleAnalyzerPlugin({
+            analyzerMode: 'static',
+            reportFilename: options.isServer ? '../analyze/server.html' : './analyze/client.html',
+            ...pluginConfig
+          })
+        )
+      }
+
+      if (typeof nextConfig.webpack === 'function') {
+        return nextConfig.webpack(config, options)
+      }
+      return config
+    }
+  })
+}
+*/
