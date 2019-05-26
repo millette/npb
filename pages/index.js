2a45bc6593e7d6b52f9783e6864058a13fbcc3ff
Robin Millette <robin@millette.info>
2019-05-25 20:48:04 -0400

    Show db (client or server side); totally not useful.

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,41 @@
+// npm
 import Link from 'next/link'
+import jsonC from 'json-cycle'
 
-export default () => (
-  <ul>
-    <li>
-      <Link href='/b' as='/a'>
-        <a>a</a>
-      </Link>
-    </li>
-    <li>
-      <Link href='/a' as='/b'>
-        <a>b</a>
-      </Link>
-    </li>
-  </ul>
+// self
+// import PouchDB from '../lib/pouchdb-browser.js'
+import PouchDB from '../lib/pouchdb.js'
+
+// const db = PouchDB ? new PouchDB('booya') : 'SKIPPED'
+// const db = new PouchDB('booya')
+// const db = 'SKIPPED'
+
+const Index = (props) => (
+  <div>
+    <ul>
+      <li>
+        <Link href='/b' as='/a'>
+          <a>a</a>
+        </Link>
+      </li>
+      <li>
+        <Link href='/a' as='/b'>
+          <a>b</a>
+        </Link>
+      </li>
+    </ul>
+  <pre>{jsonC.stringify(props.db, null, '  ')}</pre>
+  </div>
 )
+
+Index.getInitialProps = (o) => {
+  const { req, query } = o
+  const isServer = !!req
+  console.log('getInitialProps called:', isServer ? 'server' : 'client', typeof query, query)
+  if (isServer) return query
+  return jsonC.decycle({
+    db: new PouchDB('booya')
+  })
+}
+
+export default Index
