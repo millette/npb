68de42fea7558fbfb96add48d091fcdd7b1d9c8a
Robin Millette <robin@millette.info>
2019-05-25 20:47:13 -0400

    Add server-side pouchdb.

--- server.js
+++ server.js
@@ -4,7 +4,9 @@
 const { parse } = require('url')
 
 // npm
+const jsonC = require('json-cycle')
 const match = require('micro-route/match')
+const { send } = require('micro')
 const next = require('next')
 const PouchDB = require('pouchdb-core')
 const LevelPouch = require('pouchdb-adapter-leveldb')
@@ -24,17 +26,39 @@ const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
+const isI = req => match(req, '/')
 const isA = req => match(req, '/a')
 const isB = req => match(req, '/b')
+const isZ = req => match(req, '/z')
 
 async function main (req, res) {
+  // console.log('RES-keys', Object.keys(res), res.end, typeof res.end)
   const parsedUrl = parse(req.url, true)
   const { query } = parsedUrl
 
+  if (isI(req)) {
+    return app.render(req, res, '/', { db: jsonC.decycle(db) })
+  }
+
   if (isA(req)) {
     return app.render(req, res, '/b', query)
-  } else if (isB(req)) {
+  }
+
+  if (isB(req)) {
     return app.render(req, res, '/a', query)
+  }
+
+  if (isZ(req)) {
+    // When rendering client-side, we will request the same data from this route
+      // res.json(db)
+      // return JSON.parse(jsonC.stringify(db))
+      return send(res, 200, {is: 'hello'})
+    /*
+      server.get('/_data/item', (req, res) => {
+      const itemData = api.getItem()
+      res.json(itemData)
+    })
+    */
   }
 
   return handle(req, res, parsedUrl)
