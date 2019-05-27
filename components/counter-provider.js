3db1e860df00deebe6396c5efdb09cb1ef95fb4d
Robin Millette <robin@millette.info>
2019-05-27 00:12:50 -0400

    Use state hook.

--- components/counter-provider.js
+++ components/counter-provider.js
@@ -1,49 +1,23 @@
-// import React, { createContext, Component } from 'react'
-import { createContext, Component } from 'react'
+import { createContext, useState } from 'react'
 
-// const CounterContext = React.createContext()
 const CounterContext = createContext()
 
-class CounterProvider extends Component {
-  state = {
-    count: 0
-  }
-
-  increase = () => {
-    this.setState({
-      count: this.state.count + 1
-    })
-  }
-
-  increaseBy = val => {
-    this.setState({
-      count: this.state.count + val
-    })
-  }
-
-  decrease = () => {
-    this.setState({
-      count: this.state.count - 1
-    })
-  }
-
-  render () {
-    return (
-      <CounterContext.Provider
-        value={{
-          count: this.state.count,
-          increase: this.increase,
-          decrease: this.decrease,
-          increaseBy: this.increaseBy
-        }}
-      >
-        {this.props.children}
-      </CounterContext.Provider>
-    )
-  }
+const CounterProvider = (props) => {
+  const [count, setCount] = useState(0)
+  return (
+    <CounterContext.Provider
+      value={{
+        count,
+        increase: () => setCount(count + 1),
+        decrease: (val) => setCount(count + val),
+        increaseBy: () => setCount(count - 1)
+      }}
+    >
+      {props.children}
+    </CounterContext.Provider>
+  )
 }
 
-/* then make a consumer which will surface it */
 const CounterConsumer = CounterContext.Consumer
 
 export default CounterProvider
