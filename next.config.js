0869dea087fb0f9693655cbdf20ebe63cc8f9d89
Robin Millette <robin@millette.info>
2019-05-31 14:58:22 -0400

    Config cleanup.

--- next.config.js
+++ next.config.js
@@ -28,30 +28,3 @@ module.exports = withBundleAnalyzer({
     return config
   },
 })
-
-/*
-// @next/bundle-analyzer would be something like:
-
-module.exports = (pluginConfig = {}) => (nextConfig = {}) => {
-  const { enabled = true } = pluginConfig
-  return Object.assign({}, nextConfig, {
-    webpack (config, options) {
-      if (enabled) {
-        const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
-        config.plugins.push(
-          new BundleAnalyzerPlugin({
-            analyzerMode: 'static',
-            reportFilename: options.isServer ? '../analyze/server.html' : './analyze/client.html',
-            ...pluginConfig
-          })
-        )
-      }
-
-      if (typeof nextConfig.webpack === 'function') {
-        return nextConfig.webpack(config, options)
-      }
-      return config
-    }
-  })
-}
-*/
