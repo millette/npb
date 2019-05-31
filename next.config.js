927382c62b26606fd04f5095213c56ed0d4c1389
Robin Millette <robin@millette.info>
2019-05-31 12:09:45 -0400

    Use dotenv-webpack.

--- next.config.js
+++ next.config.js
@@ -1,10 +1,33 @@
+// core
+const { join } = require("path")
+
+// npm
+const Dotenv = require("dotenv-webpack")
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
   // would be nice to support more webpack-bundle-analyzer options like:
   openAnalyzer: false,
   generateStatsFile: true,
 })
-module.exports = withBundleAnalyzer({})
+
+const path = join(__dirname, ".env")
+
+module.exports = withBundleAnalyzer({
+  webpack: (config) => {
+    const dots = new Dotenv({
+      path,
+      systemvars: true,
+      safe: true,
+      defaults: true,
+    })
+    if (config.plugins) {
+      config.plugins.push(dots)
+    } else {
+      config.plugins = [dots]
+    }
+    return config
+  },
+})
 
 /*
 // @next/bundle-analyzer would be something like:
