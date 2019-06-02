612f039f7dab3b0179a97957006a96706f7ca915
Robin Millette <robin@millette.info>
2019-06-02 13:03:04 -0400

    Replace frontpage with list of pages.

--- pages/index.js
+++ pages/index.js
@@ -2,53 +2,50 @@
 import Link from "next/link"
 
 // self
-import { CounterConsumer } from "../components/counter-provider"
+import { db } from "../components/db-provider"
 
-const Index = (props) => (
-  <>
-    <h1>HOME</h1>
-    <CounterConsumer>
-      {({ count, increase, decrease }) => (
-        <div>
-          <p>Counter: {Object.keys(count).join(", ")}</p>
-          <button onClick={increase}>Increase</button>
-          <button onClick={decrease}>Decrease</button>
-        </div>
-      )}
-    </CounterConsumer>
+const Pages = ({ rows }) => (
+  <div>
+    <h1>Pages</h1>
+    {rows.map(({ doc: { _id, _rev, name, ...rest } }) => (
+      <div key={_id}>
+        <h2>
+          <Link href={`/db3?id=${_id}`} as={`/db3/${_id}`}>
+            <a>{name || <i>{_id}</i>}</a>
+          </Link>
+        </h2>
+        <dl>
+          <dt>id</dt>
+          <dd>{_id}</dd>
+          <dt>rev</dt>
+          <dd>{_rev}</dd>
+          {rest && (
+            <>
+              <dt>rest</dt>
+              <dd>
+                <pre>{JSON.stringify(rest, null, "  ")}</pre>
+              </dd>
+            </>
+          )}
+        </dl>
+      </div>
+    ))}
+
     <ul>
+      <li>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+      </li>
       <li>
         <Link href="/pages">
           <a>Pages</a>
         </Link>
       </li>
     </ul>
-  </>
-)
-
-//     <pre>{jsonC.stringify(props.alldbs, null, '  ')}</pre>
-
-// const db = PouchDB ? new PouchDB('booya') : 'SKIPPED'
-// const db = new PouchDB('booya')
-// const db = 'SKIPPED'
-/*
-const Index = (props) => (
-  <div>
   </div>
 )
-*/
 
-/*
-Index.getInitialProps = (o) => {
-  const { req, query } = o
-  const isServer = !!req
-  console.log('getInitialProps called:', isServer ? 'server' : 'client', typeof query, query)
-  if (isServer) return query
+Pages.getInitialProps = () => db.allDocs({ include_docs: true })
 
-  // const db = new PouchDB('booya')
-  return PouchDB.allDbs()
-    .then((alldbs) => ({alldbs}))
-}
-*/
-
-export default Index
+export default Pages
