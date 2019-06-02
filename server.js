46cd279e69a0e8d6acb8d0edd5c4d534c6b49307
Robin Millette <robin@millette.info>
2019-06-02 14:03:11 -0400

    Cleanup dead code, extra deps, etc.

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 "use strict"
 
 // core
-// const { parse } = require("url")
-// const { URL } = require("url")
 const { URL } = require("url")
 
 // npm
@@ -11,19 +9,19 @@ const match = require("micro-route/match")
 const { json } = require("micro")
 // const { send } = require('micro')
 const next = require("next")
-const allDbs = require("pouchdb-all-dbs")
+// const allDbs = require("pouchdb-all-dbs")
 const PouchDB = require("pouchdb-core")
 const LevelPouch = require("pouchdb-adapter-leveldb")
-const HttpPouch = require("pouchdb-adapter-http")
-const mapreduce = require("pouchdb-mapreduce")
+// const HttpPouch = require("pouchdb-adapter-http")
+// const mapreduce = require("pouchdb-mapreduce")
 // const replication = require('pouchdb-replication')
 
 PouchDB.plugin(LevelPouch)
-  .plugin(HttpPouch)
-  .plugin(mapreduce)
+// .plugin(HttpPouch)
+// .plugin(mapreduce)
 // .plugin(replication)
 
-allDbs(PouchDB)
+// allDbs(PouchDB)
 
 const q2o = (u) => {
   const o = {}
@@ -33,122 +31,38 @@ const q2o = (u) => {
   return o
 }
 
-// console.log("DB_NAME:", process.env.DB_NAME)
-
 const db = new PouchDB(process.env.DB_NAME)
-
-// console.log(db)
-/*
-db.allDocs({ include_docs: true })
-  .then(({ rows }) => {
-    // console.log(rows.map(({ doc }) => doc))
-    return PouchDB.allDbs()
-  })
-  .then(console.log)
-*/
-
-/*
-const allDbs = require("pouchdb-all-dbs")
-const PouchDB = require('pouchdb-core')
-const LevelPouch = require('pouchdb-adapter-leveldb')
-const HttpPouch = require('pouchdb-adapter-http')
-const mapreduce = require('pouchdb-mapreduce')
-const replication = require('pouchdb-replication')
-
-PouchDB.plugin(LevelPouch)
-  .plugin(HttpPouch)
-  .plugin(mapreduce)
-  .plugin(replication);
-
-allDbs(PouchDB)
-*/
-
-// const db = new PouchDB('hiha')
 
 const dev = process.env.NODE_ENV !== "production"
 
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
-// const isI = (req) => match(req, "/")
-const isA = (req) => match(req, "/a")
-const isB = (req) => match(req, "/b")
 const is3 = (req) => {
   const o = match(req, "/db3/:id")
-  // console.log('MATCH-O', o)
-  if (!o) return
-  return o.params
+  return o && o.params
+  // if (!o) return
+  // return o.params
 }
 
 const isPut = (req) => match(req, "/db1", ["PUT"])
 
 async function main(req, res) {
-  // console.log('REQ-keys', Object.keys(req))
-  // const parsedUrl = parse(req.url, true)
-  // const { query } = parsedUrl
-  // const query = q2o(parsedUrl.query)
   const query = q2o(req.url)
 
-  // const u = new URL(req.url)
-
-  /*
-  if (isI(req)) {
-    return PouchDB.allDbs()
-      .then((alldbs) => app.render(req, res, '/', { alldbs }))
-  }
-  */
-
   if (isPut(req)) {
-    // console.log("PUT", Object.keys(req))
-    // return db.put()
-
+    return json(req).then(db.put)
+    /*
     return json(req).then((j) => {
-      // console.log(j)
       return db.put(j)
     })
-  }
-
-  if (isA(req)) {
-    return app.render(req, res, "/b", query)
-  }
-
-  if (isB(req)) {
-    return app.render(req, res, "/a", query)
+    */
   }
 
   const abc = is3(req)
   if (abc) {
-    // console.log('IS-DB3', query, abc)
-
-    /*
-    const oy = match(req, '/db3/:id')
-    console.log('OY:', oy)
-    console.log('OY.params:', oy.params)
-    console.log('OY.query:', oy.query)
-    // { params, query } = oy
-    */
-
     return app.render(req, res, "/db3", abc)
   }
-
-  /*
-  const parsedUrl = {
-    ...new URL(req.url),
-    query
-  }
-
-  console.log('parsedUrl', parsedUrl)
-
-  return handle(req, res, parsedUrl)
-  */
-  return handle(req, res)
-  // return handle(req, res, req.url)
-  /*
-  return handle(req, res, {
-    ...new URL(req.url),
-    query
-  })
-  */
 }
 
 async function setup(handler) {
