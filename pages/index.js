eca938615e43496ca3740b87566286367bbd9941
Robin Millette <robin@millette.info>
2019-06-03 08:48:26 -0400

    Move new-doc link up.

--- pages/index.js
+++ pages/index.js
@@ -8,29 +8,40 @@ import { db } from "../components/db-provider"
 const Pages = ({ rows }) => (
   <div>
     <h1>Pages</h1>
-    {rows.map(({ doc: { _id, _rev, name, ...rest } }) => (
-      <div key={_id}>
-        <h2>
-          <Link href={`/db3?id=${_id}`} as={`/db3/${_id}`}>
-            <a>{name || <i>{_id}</i>}</a>
-          </Link>
-        </h2>
-        <dl>
-          <dt>id</dt>
-          <dd>{_id}</dd>
-          <dt>rev</dt>
-          <dd>{_rev}</dd>
-          {Object.keys(rest).length > 0 && (
-            <>
-              <dt>rest</dt>
-              <dd>
-                <pre>{stringify(rest, { space: 2 })}</pre>
-              </dd>
-            </>
-          )}
-        </dl>
-      </div>
-    ))}
+    <ul>
+      <li>
+        <Link href="/new-doc">
+          <a>New doc</a>
+        </Link>
+      </li>
+    </ul>
+    {rows.length ? (
+      rows.map(({ doc: { _id, _rev, name, ...rest } }) => (
+        <div key={_id}>
+          <h2>
+            <Link href={`/db3?id=${_id}`} as={`/db3/${_id}`}>
+              <a>{name || <i>{_id}</i>}</a>
+            </Link>
+          </h2>
+          <dl>
+            <dt>id</dt>
+            <dd>{_id}</dd>
+            <dt>rev</dt>
+            <dd>{_rev}</dd>
+            {Object.keys(rest).length > 0 && (
+              <>
+                <dt>rest</dt>
+                <dd>
+                  <pre>{stringify(rest, { space: 2 })}</pre>
+                </dd>
+              </>
+            )}
+          </dl>
+        </div>
+      ))
+    ) : (
+      <div>No docs.</div>
+    )}
   </div>
 )
 
